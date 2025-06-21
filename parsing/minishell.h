08f7d6088b2ab1be1357735438ed1dacaf4f5080
AbounnasrYoussef <abounnasryoussef@gmail.com>
2025-06-21 08:54:43 +0100

    chi t9abi f syntaxe

--- parsing/minishell.h
+++ parsing/minishell.h
@@ -6,7 +6,7 @@
 /*   By: yabounna <yabounna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 09:35:05 by yabounna          #+#    #+#             */
-/*   Updated: 2025/06/19 09:36:57 by yabounna         ###   ########.fr       */
+/*   Updated: 2025/06/20 12:58:36 by yabounna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ void printf_error(int x);
 void syntaxe_errors(char *args);
 int error_pipe(char *caracter , int *i);
 int error_redir(char *caracter , int *i);
-int error_quote(char *caracter);
+int error_quote(char *caracter, int *i);
 
 
 // utils
--- parsing/syntaxe_errors/error_quote.c
+++ parsing/syntaxe_errors/error_quote.c
@@ -6,31 +6,30 @@
 /*   By: yabounna <yabounna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 09:08:48 by yabounna          #+#    #+#             */
-/*   Updated: 2025/06/19 09:37:02 by yabounna         ###   ########.fr       */
+/*   Updated: 2025/06/20 13:00:04 by yabounna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
 
-int error_quote(char *caracter)
+int error_quote(char *caracter , int  *i)
 {
     char quote;
-    int i = 0;
-    while (caracter[i] != '\0')
+    while (caracter[*i] != '\0')
     {
-        if (caracter[i] == '\'' || caracter[i] == '\"')
+        if (caracter[*i] == '\'' || caracter[*i] == '\"')
         {
-            quote = caracter[i++];
-            while (caracter[i] != '\0' && caracter[i] != quote)
-                i++;
-            if (!caracter[i])
+            quote = caracter[(*i)++];
+            while (caracter[*i] != '\0' && caracter[*i] != quote)
+                (*i)++;
+            if (!caracter[*i])
             {
                 write(2, "minishell: syntax error\n", 25);
                 return -1;
             }
         }
-        i++;
+        (*i)++;
     }
     return 1;
 }
