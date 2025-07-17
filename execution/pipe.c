e8c952b95cb89879066eca834315c7290b40cb87
Atmane Rahhab <arahhab@e3r5p4.1337.ma>
2025-07-17 03:43:25 +0100

    shshsdhdhd

---
+++ execution/pipe.c
@@ -0,0 +1,191 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipe.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arahhab <arahhab@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/16 22:36:08 by arahhab           #+#    #+#             */
+/*   Updated: 2025/07/17 03:40:42 by arahhab          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "execution.h"
+
+char *ft_concat(char *str, char *str2)
+{
+	int l1;
+	int l2;
+	int i;
+	int j;
+	char *new_str;
+
+	l1 = ft_strlen(str);
+	l2 = ft_strlen(str2);
+	new_str = malloc((l1 + l2 + 1) * sizeof(char));
+	i = 0;
+	j = 0;
+	while (str[i] != '\0')
+	{
+		new_str[i] = str[i];
+		i++;
+	}
+	while(str2[j] != '\0')
+	{
+		new_str[i] = str2[j];
+		i++;
+		j++;
+	}
+	new_str[i] = '\0';
+	return new_str;
+}
+
+char *cherche_path_cmd(char *cmd, char **env)
+{
+	t_list *envv;
+	char *path;
+	char **paths;
+	char *path_cmd;
+	int i;
+	
+	envv = ft_env(env);
+	path = NULL;
+	i = 0;
+	
+	while (envv != NULL)
+	{
+		if (ft_strcmp(envv->variable, "PATH") == 0)
+		{
+			path = envv->valeur_vari;
+		}
+		envv = envv->next;
+	}
+	paths = ft_split(path, ':');
+	while (paths && paths[i] && (paths[i] != NULL))
+	{
+		paths[i] =  ft_concat(paths[i], "/");
+		paths[i] =  ft_concat(paths[i], cmd);
+		if (access(paths[i], X_OK) == 0)
+		{
+			path_cmd = paths[i];
+		}
+		i++;
+	}
+	return path_cmd;
+}
+
+//int main(int argc, char **argv, char **env)
+//{
+//	char *cmd;
+	
+//	cmd = NULL;
+//	if (argv[1] != NULL)
+//	{
+//		cmd = cherche_path_cmd(argv[1], env);
+//	}
+//	execve(cmd, &argv[1], NULL);
+//	printf("\n\n\n");
+//	execve(cmd, &argv[1], NULL);
+	
+//}
+
+//int main(int argc, char **argv, char **env)
+//{
+//	int n = 1;
+	
+//	int id;
+
+//	id = fork();
+//	if (id == 0)
+//	{
+//		n = 9;
+//	}
+//	else
+//	{
+//		n = 100;
+//	}
+//	if (id != 0)
+//	{
+//		wait(NULL);
+//	}
+//	printf("%d \n", n);
+	
+//}
+
+
+
+//int main(int argc, char **argv, char **env)
+//{
+//	char *cmd;
+	
+//	int id;
+//	cmd = NULL;
+//	if (argv[1] != NULL)
+//	{
+//		cmd = cherche_path_cmd(argv[1], env);
+//	}
+	
+//	id = fork();
+//	if (id == 0)
+//	{
+//		execve(cmd, &argv[1], NULL);
+//	}
+//	else
+//	{
+//		execve(cmd, &argv[1], NULL);
+//	}
+//	if (id != 0)
+//	{
+//		wait(NULL);
+//	}
+//}
+
+int main(int argc, char **argv, char **env)
+{
+	char *cmd;
+	char *cmd2;
+	int fd[2];
+	int pip;
+	
+	pip = pipe(fd);
+	int id;
+	int id2;
+	cmd = NULL;
+	if (argv[1] != NULL)
+	{
+		cmd = cherche_path_cmd(argv[1], env);
+	}
+	if (argv[2] != NULL)
+	{
+		cmd2 = cherche_path_cmd(argv[2], env);
+	}
+	
+	id = fork();
+	
+	if (id == 0)
+	{
+		dup2(fd[0], STDIN_FILENO);
+		close(fd[1]);
+		close(fd[0]);
+		execve(cmd, &argv[1], NULL);
+	}
+	if (id != 0)
+	{
+		wait(NULL);
+	}
+	
+	id2 = fork();
+	if (id2 == 0)
+	{
+		dup2(fd[1], STDOUT_FILENO);
+		close(fd[1]);
+		close(fd[0]);
+		execve(cmd2, &argv[2], NULL);
+	}
+	
+	if (id2 != 0)
+	{
+		wait(NULL);
+	}
+}
+
--- execution/utili_1.c
+++ execution/utili_1.c
@@ -6,7 +6,7 @@
 /*   By: arahhab <arahhab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 23:19:37 by arahhab           #+#    #+#             */
-/*   Updated: 2025/07/02 15:56:13 by arahhab          ###   ########.fr       */
+/*   Updated: 2025/07/16 22:55:05 by arahhab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ int	ft_strcmp(const char *s1, const char *s2)
 	size_t	i;
 
 	i = 0;
-	while (s1[i] || s2[i])
+	while (s1 && s2 && (s1[i] || s2[i]))
 	{
 		if ((unsigned char)s1[i] != (unsigned char)s2[i])
 			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
