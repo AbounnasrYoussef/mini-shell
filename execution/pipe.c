bb808aba99f60753ecdaf99e0b6add8c79032d47
Atmane Rahhab <arahhab@e3r8p2.1337.ma>
2025-07-18 15:29:48 +0100

    4em j

--- execution/pipe.c
+++ execution/pipe.c
@@ -6,7 +6,7 @@
 /*   By: arahhab <arahhab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 22:36:08 by arahhab           #+#    #+#             */
-/*   Updated: 2025/07/18 14:34:56 by arahhab          ###   ########.fr       */
+/*   Updated: 2025/07/18 15:28:51 by arahhab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,118 +47,40 @@ char *cherche_path_cmd(char *cmd, char **env)
 	char **paths;
 	char *path_cmd;
 	int i;
+	char **cmd_n_op;
 	
+	cmd_n_op = ft_split(cmd, ' '); 
 	envv = ft_env(env);
 	path = NULL;
 	i = 0;
-	
+	path_cmd = NULL;
 	while (envv != NULL)
 	{
 		if (ft_strcmp(envv->variable, "PATH") == 0)
 		{
 			path = envv->valeur_vari;
 		}
+		
 		envv = envv->next;
 	}
 	paths = ft_split(path, ':');
 	while (paths && paths[i] && (paths[i] != NULL))
 	{
 		paths[i] =  ft_concat(paths[i], "/");
-		paths[i] =  ft_concat(paths[i], cmd);
+		paths[i] =  ft_concat(paths[i], cmd_n_op[0]);
 		if (access(paths[i], X_OK) == 0)
 		{
 			path_cmd = paths[i];
 		}
 		i++;
 	}
+	if (path_cmd == NULL)
+	{
+		printf("%s: command not found\n", cmd);
+		exit(1);
+	}
 	return path_cmd;
 }
-
-
-//int main(int argc, char **argv, char **env)
-//{
-//	int n = 1;
-	
-//	int id;
-
-//	id = fork();
-//	if (id == 0)
-//	{
-//		n = 9;
-//	}
-//	else
-//	{
-//		n = 100;
-//	}
-//	if (id != 0)
-//	{
-//		wait(NULL);
-//	}
-//	printf("%d \n", n);
-	
-//}
-
-
-
-
-
-//int main(int argc, char **argv, char **env)
-//{
-//	char *cmd;
-//	char *cmd2;
-//	int fd[2];
-//	int pip;
-//	char *argv1[] = {argv[1], NULL};
-//	char *argv2[] = {argv[2], NULL};
-	
-//	pip = pipe(fd);
-//	int id;
-//	int id2;
-//	cmd = NULL;
-//	cmd2 = NULL;
-//	if (argc > 2)
-//	{
-//		if (argv[1] != NULL)
-//		{
-//			cmd = cherche_path_cmd(argv[1], env);
-//		}
-//		if (argv[2] != NULL)
-//		{
-//			cmd2 = cherche_path_cmd(argv[2], env);
-//		}
-		
-//		id = fork();
-		
-//		if (id == 0)
-//		{
-//			dup2(fd[1], STDOUT_FILENO);
-//			close(fd[0]);
-//			close(fd[1]);
-//			execve(cmd, argv1, NULL);
-//		}
-//		if (id != 0)
-//		{
-//			waitpid(id, NULL, 0);
-//		}
-		
-//		id2 = fork();
-//		if (id2 == 0)
-//		{
-//			dup2(fd[0], STDIN_FILENO);
-//			close(fd[0]);
-//			close(fd[1]);
-//			execve(cmd2, argv2, NULL);
-//		}
-		
-//		if (id2 != 0)
-//		{
-//			waitpid(id2, NULL, 0);
-//		}
-//		//close(fd[0]);
-//		//close(fd[1]);
-//	}	
-//}
-
 
 void ft_pipe(int argc, char **argv, char **env)
 {
@@ -166,30 +88,23 @@ void ft_pipe(int argc, char **argv, char **env)
 	int id[argc - 1];
 	int pip;
 	int i;
-	int status;
-	char **cmdy;
-
-	i = 0;
-	
-	cmd = malloc(argc * sizeof(char *));
-	while (i < (argc - 1))
-	{
-		cmdy = ft_split(argv[i + 1], ' '); 
-		cmd[i] = cherche_path_cmd(cmdy[0], env);
-		i++;
-	}
-	
-	i = 0;
-	
 	int fd[2];
 	int tmp[2];
-
+	int c;
+	
+	c = 0;
+	i = 0;
+	cmd = malloc(argc * sizeof(char *));
 	fd[0] = 0;
 	fd[1] = 1;
 	tmp[0] = 0;
 	tmp[1] = 1;
-	int j = 1;
 	while (i < argc) {
+		if (i < argc - 1)
+		{
+			cmd[i] = cherche_path_cmd(argv[i + 1], env);
+			//printf("%s \n\n", argv[i + 1]);
+		}
 		pipe(fd);
 		id[i] = fork();
 		if (id[i] == 0) {
@@ -219,14 +134,11 @@ void ft_pipe(int argc, char **argv, char **env)
 	{
 		close(tmp[0]);
 	}
-	int c;
-	c = 0;
 	while (c < argc - 1)
 	{
 		waitpid(id[c], NULL, 0);
 		c++;
 	}
-
 }
 
 
