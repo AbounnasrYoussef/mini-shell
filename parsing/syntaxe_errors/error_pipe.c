f9a68b21bf15bf9c110571ec2c5a366e1b2a4d71
AbounnasrYoussef <abounnasryoussef@gmail.com>
2025-08-14 20:04:32 +0100

    |dafgfsg=syntaxe error

--- parsing/syntaxe_errors/error_pipe.c
+++ parsing/syntaxe_errors/error_pipe.c
@@ -6,42 +6,11 @@
 /*   By: yabounna <yabounna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/20 12:59:42 by yabounna          #+#    #+#             */
-/*   Updated: 2025/08/08 12:51:38 by yabounna         ###   ########.fr       */
+/*   Updated: 2025/08/14 19:26:42 by yabounna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
-
-// static int	print_err(void)
-// {
-// 	write(2, "minishell: syntax error\n", 25);
-// 	ft_exit_status(258, 1);
-// 	return (-1);
-// }
-
-// int	error_pipe(char *caracter, int *i)
-// {
-// 	int	tmp;
-
-// 	(*i)++;
-// 	while (caracter[*i] && skip_space(caracter[*i]))
-// 		(*i)++;
-// 	if (!caracter[*i] || caracter[*i] == '|')
-// 		print_err();
-// 	if (caracter[*i] == '<' || caracter[*i] == '>')
-// 	{
-// 		tmp = *i;
-// 		if (caracter[tmp + 1] == caracter[tmp])
-// 			tmp++;
-// 		tmp++;
-// 		while (caracter[tmp] && skip_space(caracter[tmp]))
-// 			tmp++;
-// 		if (!caracter[tmp] || caracter[tmp] == '|'
-// 			|| caracter[tmp] == '<' || caracter[tmp] == '>')
-// 			print_err();
-// 	}
-// 	return (1);
-// }
 
 static int	print_err(void)
 {
@@ -50,10 +19,24 @@ static int	print_err(void)
 	return (-1);
 }
 
+int	premier_pipe(char *line)
+{
+	int	i;
+
+	i = 0;
+	while (skip_space(line[i]))
+		i++;
+	if (line[i] == '|')
+		return (1);
+	return (0);
+}
+
 int	error_pipe(char *line, int *i)
 {
 	int	tmp;
 
+	if (premier_pipe(line) == 1)
+		return (print_err());
 	(*i)++;
 	while (line[*i] && (line[*i] == ' ' || line[*i] == '\t'))
 		(*i)++;
